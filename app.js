fd059697488623f0bad2446df40ea976295f9afc
Luca Favarel <lucafavarel@gmail.com>
2023-05-04 21:20:40 -0300

    segunda pre-entrega

--- app.js
+++ app.js
@@ -1,87 +1,208 @@
-// VARIABLES
-const casacalakers = 20000
-const kobe6 = 35000
-const shortorlando = 15000
-const casacabulls = 25000
-// INICIA FUNCION
+// // VARIABLES
+// const casacalakers = 20000
+// const kobe6 = 35000
+// const shortorlando = 15000
+// const casacabulls = 25000
+
+const casacas = [
+    {nombre: "Casaca Lakers #6", precio: 15000},
+    {nombre: "Casaca Warriors #30", precio: 15000},
+    {nombre: "Casaca Celtics #0", precio: 12000},
+    {nombre: "Casaca Bulls retro MJ", precio: 20000},
+    {nombre: "Casaca Lakers retro Kobe", precio: 20000},
+]
+
+const zapatillas = [
+    {nombre: "Nike Kobe 6 protro Grinch", precio: 100000},
+    {nombre: "Nike Kobe 11", precio: 100000},
+    {nombre: "Under Armour Curry 4", precio: 80000},
+    {nombre: "Under Armour Curry 8", precio: 95000},
+    {nombre: "LeBron Soldier XIX", precio: 90000},
+]
+
+const articulos = casacas.concat(zapatillas)
+console.log(articulos)
+
+
+
+
+
+let carrito = []
+
+
+
+// // INICIA FUNCION
 
 pedirdatos ()
 
 function pedirdatos (){
-    let nombre = prompt ("Ingrese su nombre")
-    let apellido = prompt ("Ingrese su apellido")
-    let nombrecompleto = `${nombre} ${apellido}`
-    if (nombre != "" && apellido != "") {
-        alert (`Bienvenido ${nombrecompleto}`)
+    let nombre = prompt ("Ingrese su nombre completo")
+    if (nombre != "") {
+        alert (`Bienvenido a Jellys ${nombre}`)
     } else {
         alert ("faltan datos, intente de nuevo")
     }
-    console.log (nombrecompleto)
+    console.log (nombre)
+    
 }
 
-// COMPRA
+// // COMPRA
 
-let total = 0
 
-function compra (){
-    const articulos = prompt (`¿Que articulo desea comprar? Seleccione el N° de el objeto:
-    1_Casaca de Lakers $${casacalakers}
-    2_Zapatillas Kobe6 $${kobe6}
-    3_Shorts Orlando retro $${shortorlando}
-    4_Casaca Chicago Bulls retro $${casacabulls}`)
 
-    switch (articulos) {
-        case "1":
-            total += casacalakers
-            break;
-        case "2":
-            total += kobe6
-            break;
-        case "3":
-            total += shortorlando
-            break;
-        case "4":
-            total += casacabulls
-            break;
-    }
-    segundacompra()
-}
+
+
+
 compra()
+function compra (){
+    const opcion =  prompt("Tenemos en stock algunas casacas y algunas zapatillas, presione 1 para ver las casacas o presione 2 para ver las zapatillas")
+switch (opcion) {
+    case "1":
+        let disponible = `Estas son nuestar casacas disponibles:\n`
+        for (let i = 0; i < casacas.length; i++) {
+            disponible += casacas[i].nombre + " $" + casacas[i].precio + "\n"
+        }
+        alert (disponible)
+        compra2()
+        break;
+    case "2":
+        let zapatillasstock = `Estas son nuestar zapatillas disponibles:\n`
+        for (let i = 0; i < zapatillas.length; i++) {
+            zapatillasstock += zapatillas[i].nombre + " $" + zapatillas[i].precio + "\n"
+        }
+        alert (zapatillasstock)
+        compra2()
+        break;
 
+}
+}
 
-function segundacompra (){
-    const compra2 = prompt ("¿Desea comprar algo más?")
-
-    if (compra2 == "si") {
+function compra2() {
+    let decision = prompt ("Escriba el nombre completo del articulo que quiera comprar, en caso contraio escriba NO para volver al stock")
+    for (let i = 0; i < articulos.length; i++) {
+        if (articulos[i].nombre === decision) {
+            console.log (articulos[i])
+            alert("Su articulo se agregó con exito")
+            carrito.push (articulos[i])
+            console.log (carrito)
+        }
+    }
+    if (decision == "no") {
         compra()
     }
-    if (compra2 == "no"){
-        alert(`Su total de compra es de $${total}`)
-        pagar()
+    compra3()
+}
+
+function compra3() {
+    const pregunta = prompt ("Desea comprar algo más?")
+    switch (pregunta) {
+        case "si":
+            compra2()
+            break;
+        case "no":
+            alert("Ahora proseguiremos con el pago")
+            pago1()
+            break;
+        pago1()
     }
 }
 
-function pagar(){
-    let totalcuotas3 = total/3
-    let totalcuotas6 = total/6
-    const pago = prompt (`Como desea abonar su compra? Seleccione el N° de su elección:
-    1_ Un solo pago
-    2_ 3 coutas con 3% de recargo
-    3_ 6 cuotas con 8% de recargo`)
-    switch (pago){
-        case "1":
-            alert ("Muchas gracias por comprar, espero que vuelva pronto")
+function pago1() {
+    let totalcarrito = carrito.reduce ((acumulado, articulos) => {
+        return acumulado + articulos.precio;
+    },0 )
+    console.log(totalcarrito)
+    let totalcarritoalert = `Carrito:\n`
+    totalcarritoalert += "$" + totalcarrito
+    alert(totalcarritoalert)
+    confirmacion()
+}
+function confirmacion() {
+    const confirmacion = prompt ("Desea confirmar la compra?") 
+    switch (confirmacion) {
+        case "si":
+            alert (`Muchas Gracias por su compra, espero verlo nuevamente`)
             break;
-        case "2":
-            alert (`Las cuotas seran de $${totalcuotas3.toFixed(2)} con un 3% de interes`)
-            alert ("Muchas gracias por comprar, espero que vuelva pronto")
-            break;
-        case "3":
-            alert (`Las cuotas seran de $${totalcuotas6.toFixed(2)} con un interes de 6%`)
-            alert ("Muchas gracias por comprar, espero que vuelva pronto")
+        case "no":
+            alert ("Gracias por su visita, espero que vuelva pronto")
             break;
     }
 }
 
 
+
+// PRE-ENTREGA 2
+
+// arrays
+
+
+
+
+
+// const accesorios = [
+//     {nombre: "Vincha Nike", precio: 3000},
+//     {nombre: "Manga Nike", precio: 4000},
+//     {nombre: "Calza corto Under Armour", precio: 10000},
+//     {nombre : "Calza Nike", precio: 12000},
+// ]
+
+
+// console.log(totalcarrito)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// if (mostrar === "2") {
+//     let disponible = "Zapatillas disponibles: \n"
+//        for (let index = 0; index < zapatillas.length; index++) {
+//     disponible += zapatillas[index].nombre + " $" + zapatillas[index].precio + "\n";
+//    }
+//    alert(disponible)
+// }
+
+// if (mostrar === "3") {
+//     let disponible = "Accesorios disponibles: \n"
+//        for (let index = 0; index < accesorios.length; index++) {
+//     disponible += accesorios[index].nombre + " $" + accesorios[index].precio + "\n";
+//    }
+//    alert(disponible)
+// }
+
+
+
+
+
+
+
+
+
+// const elegir = prompt(` ¿Que desea comprar? Seleccione el número correspondiente
+// 1_${casacas[0].nombre} : ${casacas[0].precio}
+// 2_${casacas[1].nombre} : ${casacas[1].precio}
+// 3_${casacas[2].nombre} : ${casacas[2].precio}
+// 4_${casacas[3].nombre} : ${casacas[3].precio}
+// 5_${casacas[4].nombre} : ${casacas[4].precio}`);
 
